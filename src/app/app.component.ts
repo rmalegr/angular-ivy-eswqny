6d8a963b29bdc03cd38d0dfd2d2ec77ebb009c6e
rmalegr <95239100+rmalegr@users.noreply.github.com>
2022-12-12 16:12:00 -0300

    2 files modified, practica interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+import getUAString from '@popperjs/core/lib/utils/userAgent';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  nombre: string = 'Rodrigo';
+  textoPlaceHolder: string = 'Ingrese algo aqui';
+  desabilitado: boolean = true;
+  constructor() {}
+
+  getSuma(numero1: number, numero2: number) {
+    return numero1 + numero2;
+  }
 }

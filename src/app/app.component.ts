a636047a80325db822d85d542db513390f61173b
rmalegr <95239100+rmalegr@users.noreply.github.com>
2022-12-12 22:58:57 -0300

    1 file modified, ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
   nombre: string = 'Rodrigo';
   textoPlaceHolder: string = 'Ingrese algo aqui';
+  texto: string = 'Este es un video event Binding';
+  texto1: string = '';
   desabilitado: boolean = true;
   imgSrc =
     'https://thumbs.gfycat.com/OffbeatViciousHousefly-size_restricted.gif';
@@ -19,5 +21,9 @@ export class AppComponent {
 
   getSuma(numero1: number, numero2: number) {
     return numero1 + numero2;
+  }
+
+  cambiarTexto(): void {
+    this.texto = 'En el proximo video vamos a ver two data Binding';
   }
 }
